71256a0948981249bbd4dd6e93468114d520d03c
ArcangeliNicola <nicola.arcangeli3@studio.unibo.it>
2026-04-30 15:01:21 +0200

    Create numero.R

---
+++ R/numero.R
@@ -0,0 +1,23 @@
+numero<-function(x) {
+if(x>0) {
+  "questo numero è positivo"}
+else if(x<0) {
+"questo numero è negativo"}
+else {
+"Zero non è ne negativo ne positivo"}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
